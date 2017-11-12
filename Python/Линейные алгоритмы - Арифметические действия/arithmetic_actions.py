b522615ad523071f880c48a98c03d9b2ce70e2bf
san-smith <san-smith@mail.ru>
2017-11-12 23:00:22 +0700

    Решена задача на Python

---
+++ Python/Линейные алгоритмы - Арифметические действия/arithmetic_actions.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 12 22:52:32 2017
+
+@author: Smith
+
+Даны два ненулевых числа. Найти их сумму, разность, произведение и частное.
+"""
+a = float(input("a = "))
+b = float(input("b = "))
+print("{0} + {1} = {2}".format(a, b, a + b))
+print("{0} - {1} = {2}".format(a, b, a - b))
+print("{0} * {1} = {2}".format(a, b, a * b))
+print("{0} / {1} = {2}".format(a, b, a / b))
