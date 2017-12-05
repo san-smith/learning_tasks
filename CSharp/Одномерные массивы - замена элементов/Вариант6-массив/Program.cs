6093dd2edb9f1f7368b9c4f32dadbf9b40b44c71
Vakha13337 <17272yyyy@mail.ru>
2017-12-05 17:25:17 +0700

    Вариант 6

---
+++ CSharp/Одномерные массивы - замена элементов/Вариант6-массив/Program.cs
@@ -0,0 +1,49 @@
+﻿/*заменить все положительные элементы целочисленного 
+ * массива размера на значение минимального.*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Вариант6_массив
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int temp = rand.Next(1000);
+            int[] array = new int[temp];
+
+            int min = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(100);
+
+                if (i == 0)
+                {
+                    min = array[i];
+                }
+                else
+                {
+                    if (array[i] < min)
+                    {
+                        min = array[i];
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    array[i] = min;
+                }
+                Console.Write(array[i] + "  ");
+            }
+
+            Console.WriteLine(min);
+            Console.ReadLine();
+        }
+    }
+}
