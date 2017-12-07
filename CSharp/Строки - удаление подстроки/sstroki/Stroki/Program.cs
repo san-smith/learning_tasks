9c5c6f789b57e48691bc1d610f1b66da7a4adc4c
nastasya-sitnikova <nastasya.sitnikova.97@mail.ru>
2017-12-07 19:57:43 +0700

    Строки

---
+++ CSharp/Строки - удаление подстроки/sstroki/Stroki/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string str = Console.ReadLine();
+            Method q = new Method();
+            Console.WriteLine(q.SubStrDel(str));
+            Console.Read();
+        }
+
+    }
+    class Method
+    {
+        public string SubStrDel(String stroka)
+        {
+            Console.Write("Введите подстроку: ");
+            string sub = Console.ReadLine();
+            return stroka.Replace(sub, "");
+        }
+    }
+}
