71e04a45b9ceaac2119070ab944ef63edfe79f6c
nastasya-sitnikova <nastasya.sitnikova.97@mail.ru>
2017-12-11 18:40:48 +0700

    Строки

---
+++ CSharp/Строки - удаление подстроки/Строки/Строки/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Введите строку:");
+            string s2 = Console.ReadLine();
+            int indexFirst = s1.IndexOf(s2);
+            s1 = s1.Remove(indexFirst, s2.Length);
+            Console.WriteLine(s1);
+        }
+
+    }
+}
