4d7f64394a3e2237f6976d541f559a3b186234f2
Vakha13337 <33612746+Vakha13337@users.noreply.github.com>
2017-12-19 17:39:28 +0700

    Add files via upload

---
+++ CSharp/условные операторы - упорядочить по убыванию/Вариант6 - условные операторы/Program.cs
@@ -0,0 +1,64 @@
+﻿/*Значения переменных X, Y, Z поменять местами так, 
+ *чтобы они оказались упорядоченными по убыванию. */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            Console.WriteLine("Введите Х");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите Y");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите Z");
+            z = Convert.ToInt32(Console.ReadLine());
+            int vrem;
+            if ((x > y) && (y < z) && (x > z))
+            {
+                vrem = y;
+                y = z;
+                z = vrem;
+            }
+            else if ((x > y) && (y < z) && (x < z))
+            {
+                vrem = y;
+                y = z;
+                z = vrem;
+
+                vrem = y;
+                y = x;
+                x = vrem;
+            }
+            else if ((x < y) && (y > z) && (x > z))
+            {
+                vrem = y;
+                y = x;
+                x = vrem;
+            }
+            else if ((x < y) && (y > z) && (x < z))
+            {
+                vrem = y;
+                y = x;
+                x = vrem;
+
+                vrem = y;
+                y = z;
+                z = vrem;
+            }
+            else if ((x < y) && (y < z))
+            {
+                vrem = z;
+                z = x;
+                x = vrem;
+            }
+            Console.WriteLine("x = " + x + "y = " + y + "z = " + z);
+            Console.ReadLine();
+        }
+    }
+}
